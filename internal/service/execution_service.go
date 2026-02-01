d01499a313fb2778aaa5cc8a086e316979ccfce4
Alina Khorst <alina.khorst@yandex.ru>
2026-02-02 00:10:13 +0300

    add code executor

---
+++ internal/service/execution_service.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+	
+	"bytebattle/internal/executor"
+)
+
+type ExecutionService struct {
+	exec executor.Executor
+}
+
+func NewExecutionService(exec executor.Executor) *ExecutionService {
+	return &ExecutionService{exec: exec}
+}
+
+func (s *ExecutionService) Execute(ctx context.Context, req executor.ExecutionRequest) (executor.ExecutionResult, error) {
+	return s.exec.Run(ctx, req)
+}
