dcac987cf9fee5a1f7b08120250f9f03f8cd1f3a
Ilya Kataev <k4t4eff@gmail.com>
2026-03-29 09:26:47 +0300

    add indexes

---
+++ internal/migrations/000018_performance_indexes.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_games_creator_id;
+DROP INDEX IF EXISTS idx_sessions_expires_at;
---
+++ internal/migrations/000018_performance_indexes.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_games_creator_id ON games(creator_id);
+CREATE INDEX idx_sessions_expires_at ON sessions(expires_at);
